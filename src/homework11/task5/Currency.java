6fc455276258c74ac78124334b5b875481fbf045
hakobtigrani <hakob.t.sahakyan@gmail.com>
2025-08-21 18:46:58 +0400

    homework 11 task5

---
+++ src/homework11/task5/Currency.java
@@ -0,0 +1,27 @@
+package homework11.task5;
+
+public enum Currency {
+    USD("$", "United States Dollar"),
+    EUR("€", "Euro"),
+    JPY("¥", "Japanese Yen");
+
+    private final String symbol;
+    private final String country;
+
+    Currency(String symbol, String country) {
+        this.symbol = symbol;
+        this.country = country;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String toString () {
+        return this.name() + " - " + country;
+    }
+}
---
+++ src/homework11/task5/Print.java
@@ -0,0 +1,11 @@
+package homework11.task5;
+
+public class Print {
+    public static void main(String[] args) {
+
+        for (Currency c : Currency.values()) {
+            System.out.println(c);
+        }
+
+    }
+}
