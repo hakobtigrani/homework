acd7b6f488a3422a638cfbaab9cb6b18c32f4b73
hakobtigrani <hakob.t.sahakyan@gmail.com>
2025-08-30 18:27:44 +0400

    homework 12 all done

--- src/homework12/task11/StringOperations.java
+++ src/homework12/task11/StringOperations.java
@@ -13,7 +13,7 @@ public class StringOperations {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string: ");
+        System.out.println("Enter a string:");
         String input = scanner.nextLine();
 
         try {
