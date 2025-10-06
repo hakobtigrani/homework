4c9ac873e7c7161d6433b52a9f612266c0e6ef72
hakobtigrani <hakob.t.sahakyan@gmail.com>
2025-10-06 17:07:35 +0400

    .

--- src/Implement/Testing.java
+++ src/Implement/Testing.java
@@ -1,15 +1,7 @@
 package Implement;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Testing {
     public static void main(String[] args) {
-        MyArrayList cars = new MyArrayList();
-        cars.add(2);
-        cars.add(4);
-        cars.add(7);
-        cars.add(1);
-        System.out.println(Arrays.toString(cars.toArray()));
+
     }
 }
